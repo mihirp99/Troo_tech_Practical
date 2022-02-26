3cbc12e7ac36fb9acf676e1c43bfc383ebbd3bae
mihirmyriad <75715634+mihirmyriad@users.noreply.github.com>
2022-02-26 16:14:37 +0530

    changes new

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider-hero').slick({
+        vertical: true,
+        dots: true,
+        arrows:false,
+        useTransform: true,
+        cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1.000)',
+        adaptiveHeight: true,
+    });
+  });
